10f1789c58dcbfb24572b271cb051236f8aedbf3
VEGAS <934085876@qq.com>
2017-03-13 02:18:43 +0800

    new method
    new method

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -29,3 +29,20 @@ public:
         return count > maxnum ? count : maxnum;
     }
 };
+/*
+#include<unordered_map>
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+*/
